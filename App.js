b66727b18b74575a06e18ce3a13358cdff581fdb
Joshi Kumar Manne <joshikumar.manne@non.se.com>
2025-07-19 18:13:02 +0530

    Laying the Foundation

--- App.js
+++ App.js
@@ -1,6 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Hello World");
+//React Element
+const heading = (
+    <div>
+        <h1>Namaste React</h1>
+        <h2>Welcome to the world of React</h2>
+    </div>
+)
+
+const HeadingComponent = () =>{
+    return (
+        <div>
+            <h1>This is Functional Component</h1>
+        </div>
+    )
+}
+
+const AppComponent = () => {
+    return (
+        <div>
+            {HeadingComponent()} {/* same as <HeadingComponent /> */}
+            {heading}
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppComponent />);
