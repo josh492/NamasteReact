01fa8c9c9ae16117970d8964675d669346e8c3f6
Joshi Kumar Manne <joshikumar.manne@non.se.com>
2025-07-26 20:19:52 +0530

    Filter Fix

--- components/Body.js
+++ components/Body.js
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { restaurants } from "../mockdata/data";
 import Search from "./Search";
 import Shimmer from "./Shimmer";
 const Body = () => {
     const [restaurantList, setRestaurantList] = useState([]);
     const [searchText, setSearchText] = useState("");
-
-    useEffect(()=>{
-        if(searchText){
-            const filteredRestaurants = restaurants.filter(restaurant =>
-                restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())
-            );
-            setRestaurantList(filteredRestaurants);
-        }else{
-            setRestaurantList(restaurants);
-        }
-    }, [searchText])
+    const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
 
     useEffect(()=>{
         fetchData();
     }, []);
+
+    useEffect(()=>{
+        if(searchText){
+            const filteredRestaurants = restaurantList.filter(restaurant =>
+                restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredRestaurantList(filteredRestaurants);
+        }else{
+            setFilteredRestaurantList(restaurantList);
+        }
+    }, [searchText]);
 
     const fetchData = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.38430&lng=78.45830&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
@@ -28,11 +28,11 @@ const Body = () => {
         setRestaurantList(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
     }
 
-    return restaurantList.length === 0 ? <Shimmer /> : (
+    return filteredRestaurantList.length === 0 ? <Shimmer /> : (
         <div className="flex flex-col items-center m-4">
             <Search setSearchText={setSearchText}/>
             <div className="flex flex-wrap justify-center m-4">
-                {restaurantList.map((restaurant) => (
+                {filteredRestaurantList.map((restaurant) => (
                     <RestaurantCard key={restaurant.info.id} restaurant={restaurant} />
                 ))}
             </div>
